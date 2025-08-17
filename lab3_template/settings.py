a731736839673d326fbd55fb01ee356b6eb8d99c
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-17 09:21:38 +0530

    Setting up Postgres

--- lab3_template/settings.py
+++ lab3_template/settings.py
@@ -1,17 +1,22 @@
 # PostgreSQL
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '#Replace it with the generated password in Step 1#',
-        'HOST': 'postgres',
-        'PORT': '5432',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    },
 }
 
-INSTALLED_APPS = (
-    'related_objects',
-)
+INSTALLED_APPS = ("related_objects",)
 
-SECRET_KEY = 'SECRET KEY for this Django Project'
+SECRET_KEY = os.environ["SECRET_KEY"]
